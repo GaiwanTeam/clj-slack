1e9ac2e7e04e52546c9d12d15c1b7a2690a25a3b
Laurence Chen <humorless@gmail.com>
2021-06-03 15:27:06 +0800

    initial commit

---
+++ src/co/gaiwan/clj_slack.clj
@@ -0,0 +1,50 @@
+(ns co.gaiwan.clj-slack
+  (:require [io.pedestal.log :as log]
+            [co.gaiwan.slack-middleware :as mw]
+            [co.gaiwan.slack-request :as req])
+  (:gen-class))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Slack API functions
+(defn slack-conn [slack-api-url slack-bot-token]
+  {:api-url slack-api-url
+   :token slack-bot-token})
+
+(defn collection-endpoint
+  [key endpoint]
+  (mw/wrap-paginate
+   #(log/info :slack-api/pagination-error-resp %)
+   key
+   (mw/wrap-rate-limit
+    (fn self
+      ([connection]
+       (self connection {}))
+      ([connection opt]
+       (req/slack-request connection endpoint opt))))))
+
+(def get-emoji (collection-endpoint :emoji "emoji.list"))
+(def get-users (collection-endpoint :members "users.list"))
+(def get-channels (collection-endpoint :channels "conversations.list"))
+(def get-history (collection-endpoint :messages "conversations.history"))
+(def get-pins (collection-endpoint :items "pins.list"))
+
+(defn error?
+  "Is the response an error?
+  This checks for a couple of different cases that might arise. Generally it's
+  advisable to always check if a response is an error before trying to use its
+  results.
+  Slack returns an :ok true/false key in every response. For paginated
+  collection responses we normally unwrap the outer map, unless (= :ok false),
+  so this should also work on collection responses.
+  For rare exceptions where Slack returns a non-200 response with an empty body
+  we simply return `:error`."
+  [response]
+  (or (= :error response)
+      (and (map? response) (false? (:ok response)))))
+
+(defn join-channel [conn name]
+  (let [channel-id (->> (get-channels conn)
+                        (filter (comp #{name} :name_normalized))
+                        first
+                        :id)]
+    (req/slack-request conn "conversations.join" {:channel channel-id})))
---
+++ src/co/gaiwan/slack_middleware.clj
@@ -0,0 +1,48 @@
+(ns co.gaiwan.slack-middleware
+  (:gen-class))
+
+(defn wrap-rate-limit
+  "Decorator for slack request functions which handles rate limiting.
+  The resulting function will automatically block the thread and retry when
+  necessary."
+  [f]
+  (fn invoke [& args]
+    (try
+      (apply f args)
+      (catch clojure.lang.ExceptionInfo ex
+        (let [data (ex-data ex)]
+          (if (= 429 (:status data))
+            (let [wait-for (Integer/parseInt (get-in data [:headers "retry-after"]))]
+              (println "rate-limited. Retry in" wait-for "seconds")
+              (Thread/sleep (* (inc wait-for) 1000))
+              (apply invoke args))
+            (throw ex)))))))
+
+(defn wrap-paginate
+  "Decorator for slack request functions which handles pagination
+  Returns a lazy seq of the collection, or the response map if the first request
+  fails. `k` is the key slack uses in the response to denote the result
+  collection.
+  When a non first request fails, the lazy seq of the collection before that
+  request will be returned. If the error-logger in injected, the failed request's
+  response will be record by the error-logger.
+  Note: error-logger can be nil if it is not needed."
+  [error-logger k f]
+  (fn paginate
+    ([conn]
+     (paginate conn {}))
+    ([conn opts]
+     (let [opts* (assoc opts :limit "100000")
+           resp (f conn opts*)
+           lazy-f (fn lazy-f [{:keys [ok] :as resp}]
+                    (if ok
+                      (let [cursor (get-in resp [:response_metadata :next_cursor])]
+                        (lazy-cat (get resp k)
+                                  (when-not (empty? cursor)
+                                    (lazy-f (f conn (merge opts* {:cursor cursor}))))))
+                      (when error-logger
+                        (error-logger resp))))]
+       (if (:ok resp)
+         (lazy-f resp)
+         resp)))))
+
