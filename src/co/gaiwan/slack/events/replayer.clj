458f87dfe593c6221e3dab0e7e14c79a941de6e5
Felipe Barros <felipebarros@protonmail.com>
2022-07-27 05:48:49 -0300

    [WIP] Implementing offset-event
    Even though my manual tests work for the parts of offset-event, the
    function itself isn't working. Probably some nuance of walk/postwalk
    is tripping me up.

--- src/co/gaiwan/slack/events/replayer.clj
+++ src/co/gaiwan/slack/events/replayer.clj
@@ -1,6 +1,8 @@
 (ns co.gaiwan.slack.events.replayer
   "EventSource that replays pre-recorded Slack events."
-  (:require [co.gaiwan.slack.protocols :as protocols]))
+  (:require [clojure.math :as math]
+            [clojure.walk :as walk]
+            [co.gaiwan.slack.protocols :as protocols]))
 
 (defrecord ReplayerEventSource
     [sorted-events                      ; Atom of sorted sequence events.
@@ -14,16 +16,67 @@
   (remove-listener [this watch-key]
     (swap! listeners dissoc watch-key)))
 
+;;; Contextual definitions:
+;;; s :: Second
+;;; us :: Microsecond
+
+(def ts-regex #"(\d{10})\.(\d{6})")
+
+(def ts? (comp boolean (partial re-matches ts-regex)))
+
+(defn ts?->us-ts
+  "If ts is a Slack ts string, converts it to Long. Else returns nil.
+
+  The timestamps used by the Slack API are strings representing
+  a [UNIX time](https://en.wikipedia.org/wiki/Unix_time), with
+  microseconds after the decimal point."
+  [ts]
+  (when (string? ts)
+    (when-let [[_ s us] (re-matches ts-regex ts)]
+      (parse-long (str s us)))))
+
+;; (defn ts->us-ts
+;;   "Converts a Slack timestamp to a Long."
+;;   [ts]
+;;   (let [[s us] (str/split ts #"\.")]
+;;     (parse-long (str s us))))
+
+(defn left-pad
+  ([n s]
+   (left-pad \space n s))
+  ([c n s]
+   (if (<= n (count s))
+     s
+     (left-pad c n (str c s)))))
+
+(def left-pad-0-10 (partial left-pad \0 10))
+(def left-pad-0-6 (partial left-pad \0 6))
+
+(defn us-ts->s [us-ts]
+  (-> us-ts (/ 1e6) math/round str left-pad-0-10))
+
+(defn us-ts->us [us-ts]
+  (-> us-ts (mod 1e6) math/round str left-pad-0-6))
+
+(defn us-ts->ts [us-ts]
+  (let [s  (us-ts->s us-ts)
+        us (us-ts->us us-ts)]
+    (str s \. us)))
+
 (defn offset-event
   "Take a raw slack event, find all timestamps, and change them by adding offset-us, in microseconds."
   [event offset-us]
   ;; use clojure.walk/postwalk to transform the event map
-  ;; find each item that looks like a timestamp, e.g. "1614822402.022400"
-  ;; remove the dot and parse as integer (Long/parseLong ...)
-  ;; add offset-ms
-  ;; split into seconds and microseconds (seconds = divide by 1e6 and round down ; microseconds = modulo 1e6)
-  ;; turn back into a string
-  )
+  (walk/postwalk
+   ;; find each item that looks like a timestamp, e.g. "1614822402.022400"
+   (fn [x]
+     (some-> (ts?->us-ts x)
+             (+ offset-us)
+             us-ts->ts))
+   ;; add offset-us
+   ;; split into seconds and microseconds (seconds = divide by 1e6 and round down ; microseconds = modulo 1e6)
+   ;; turn back into a string
+   event))
 
 (defn from-events
   "Take a collection of raw slack events (a sequence/vector/set containing maps)
@@ -69,4 +122,15 @@
                  "user_team" "T03RZGPFR"}
                 123456789 ;; 123.456789 seconds
                 )
+
+
+  (def make-ts
+    (fn []
+      (->> #(rand-int 9)
+           repeatedly
+           (take 16)
+           (partition-all 10)
+           (map (partial apply str))
+           (clojure.string/join \.))))
+
   )
