b5d493a47cd81fbde374101519d2d41be82cb50d
Arne Brasseur <arne@arnebrasseur.net>
2022-10-18 11:14:30 +0200

    Render emoji in markdown-to-text

--- src/co/gaiwan/slack/markdown/render_text.clj
+++ src/co/gaiwan/slack/markdown/render_text.clj
@@ -1,7 +1,8 @@
 (ns co.gaiwan.slack.markdown.render-text
   "Basic \"rendering\" of markdown, replacing channel and user ids with something
   more readable"
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [co.gaiwan.slack.markdown :as markdown]))
 
 
 (defn render-text [text user-handler channel-handler]
@@ -12,7 +13,10 @@
                        (or (user-handler uid) uid)))
         (str/replace #"<#([A-Z0-9]+)(\|[^>]*)>"
                      (fn [[_ cid]]
-                       (or (channel-handler cid) cid))))))
+                       (or (channel-handler cid) cid)))
+        (str/replace #":([a-z0-9_+-]{1,55}(::skin-tone-[2-6])?):"
+                     (fn [[orig code]]
+                       (get @markdown/standard-emoji-map code orig))))))
 
 (defn enrich-message [message user-handler channel-handler]
   (assoc message
